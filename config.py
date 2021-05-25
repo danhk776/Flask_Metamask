775876ffb3aee751c413331ba0e69a96be0ca6a1
danhk776 <danielhaik@yahoo.fr>
2021-05-25 20:50:17 +0200

    add heroku database

--- config.py
+++ config.py
@@ -4,6 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'postgres://oqwuuekwxaybaa:da0d9ca64f7678944056474cc94b3922b57ed434498a5902304e1096e047eff1@ec2-54-220-170-192.eu-west-1.compute.amazonaws.com:5432/d9k7jus30tbmvc'
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
 
